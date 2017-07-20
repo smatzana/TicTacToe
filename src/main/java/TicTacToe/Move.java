62cb6db0635ba0f9c568b964c3b50ec0efce3bee
Sotos <smatzana@users.noreply.github.com>
2017-07-20 08:48:52 +0200

    Update Move.java

--- src/main/java/TicTacToe/Move.java
+++ src/main/java/TicTacToe/Move.java
@@ -1,14 +1,3 @@
-/*
- * Telefónica Digital - Product Development and Innovation
- *
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
- * All rights reserved.
- */
-
 package ticTacToe;
 
 import org.apache.commons.lang3.tuple.Pair;
